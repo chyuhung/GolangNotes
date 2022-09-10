013c8bde082f2691ddd6944a869a80b6a821ce21
chyuhung <977599667@qq.com>
2022-09-10 12:25:50 +0800

    添加示例

--- basic/pointer.go
+++ basic/pointer.go
@@ -4,6 +4,16 @@ import "fmt"
 
 // 指针
 func main() {
+	helloPointer()
+	fmt.Println()
+	// 示例
+	var num = 4
+	fmt.Printf("&num:%v\n", &num)
+	squareVal(num)
+	squareAdd(&num)
+}
+
+func helloPointer() {
 	// 创建一个数值型变量
 	i, j := 24, 15237
 	// 创建一个指针
@@ -20,4 +30,14 @@ func main() {
 	fmt.Println(&j) // j内存地址
 	fmt.Println(p)  // 赋值后变成j内存地址
 	fmt.Println(*p) //15237
+}
+
+func squareVal(v int) {
+	v *= v
+	fmt.Println(&v, v)
+}
+
+func squareAdd(p *int) {
+	*p *= *p
+	fmt.Println(p, *p)
 }
